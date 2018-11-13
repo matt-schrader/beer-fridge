52c36fbbcd4bbb5074be6d64aa49aecac05ae684
Matt Schrader <SchraderMJ11@gmail.com>
2018-11-12 20:46:44 -0600

    Respond to device with version if newer

--- src/services/messages/messages.service.js
+++ src/services/messages/messages.service.js
@@ -14,22 +14,23 @@ module.exports = function (app) {
   };
 
   const messageService = {
-    async create(data, params) {
+    async create(data, _params) {
       if (data.id) {
         var deviceService = app.service('device')
         var search = await deviceService.find({ query: { id: data.id }})
         let device
-        if (!search.data.length) {
+        if (!search.length) {
           const newDevice = { id: data.id }
           device = await deviceService.create(newDevice)
-        } else if (search.data.length === 1) {
-          device = search.data[0]
+        } else if (search.length === 1) {
+          device = search[0]
         } else {
           Promise.reject('Found more than one device matching the request!')
         }
-        console.log('device', device)
 
         const logTime = moment.utc()
+        await deviceService.patch(device._id, { lastEventTime: logTime })
+
         if (data.temp) {
           var tempService = app.service('temperature-log')
           var temperatureLog = {
@@ -51,7 +52,19 @@ module.exports = function (app) {
           await powerService.create(powerLog)
           await deviceService.patch(device._id, { currentState: data.state })
         }
-        return Promise.resolve({ status: 'Ok'})
+
+        const resultPacket = { status: 'Ok' }
+        if (data.version && data.version < device.version) {
+          resultPacket.version = device.version;
+          if (device.name) {
+            resultPacket.hostname = device.name.toLowerCase().split(' ').join('_')
+          }
+
+          if (device.targetTemperature) {
+            resultPacket.targetTemperature = device.targetTemperature
+          }
+        }
+        return Promise.resolve(resultPacket)
       }
       return Promise.reject("Invalid object")
     },
